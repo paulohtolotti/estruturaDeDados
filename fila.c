def8de0f2e5199111b749fefdaa3387746d1f160
Paulo H. <paulohtolotti@alumni.usp.br>
2025-06-07 20:22:14 -0300

    feat: adição do programa de pilhas em c, com inserção e exibição

---
+++ fila.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct temp{
+	int				key;
+	struct temp		*next;
+}member;
+
+member *head, *tail, *newMember, *aux;
+
+
+void insert(int val) {
+	newMember = (member *) malloc (sizeof(member));
+	newMember->key = val;
+	newMember->next = NULL; //Proximo do novo membro sempre é o NULL (fim da fila)
+	
+	if (head == NULL) head = newMember; //Fila vazia. O inicio vira o novo membro
+	else tail->next = newMember; //Proximo do fim vira o novo.
+	
+	tail = newMember;
+}
+
+void showQueue(){
+	aux = head;
+	while (aux != NULL){
+		printf("%i => ", aux->key);
+		aux = aux->next;
+	}
+	printf("NULL\n\n");
+}
+int main(){
+	head = NULL;	//Inicializa a fila vazia
+	insert(3); insert(1); insert(4); insert(1); insert(5);
+	showQueue();
+};
